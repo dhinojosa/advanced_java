2662f67765e20205fb6cf2ef4aba3f631f86a60f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-10-01 13:36:39 +0200

    Removed Answers in DateTimeExercises

--- src/test/java/com/xyzcorp/exercises/datetime/DateTimeExercises.java
+++ src/test/java/com/xyzcorp/exercises/datetime/DateTimeExercises.java
@@ -1,5 +1,6 @@
 package com.xyzcorp.exercises.datetime;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -17,44 +18,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DateTimeExercises {
     @Test
+    @Disabled("Fill in your answers")
     void testIterateWithThreeDays() {
-        List<LocalDate> firstSixEveryThirdDaysOf2018 =
-            Stream.iterate(LocalDate.of(2018, 1, 1),
-                localDate -> localDate.plus(3, ChronoUnit.DAYS))
-                  .limit(6).collect(Collectors.toList());
 
-        assertThat(firstSixEveryThirdDaysOf2018).contains(
-            LocalDate.of(2018, 1, 1),
-            LocalDate.of(2018, 1, 4),
-            LocalDate.of(2018, 1, 7),
-            LocalDate.of(2018, 1, 10),
-            LocalDate.of(2018, 1, 13),
-            LocalDate.of(2018, 1, 16)
-        );
     }
 
     @Test
+    @Disabled("Fill in your answers")
     void testFindAllAmericasTimeZones() {
-        ZoneId.getAvailableZoneIds().stream().
-            filter(s -> s.startsWith("America"))
-              .map(s -> Arrays.stream(s.split("/")).skip(1).collect(Collectors.joining()))
-              .forEach(System.out::println);
+
     }
 
     @Test
+    @Disabled("Fill in your answers")
     void testFindAllTimeZonesInAntarctica() {
-        LocalDateTime now = LocalDateTime.now();
-        List<ZonedDateTime> antarcticZones =
-            ZoneId.getAvailableZoneIds().stream()  // Stream<String>
-                  .filter(regionId -> regionId.contains("Antarctica"))
-                  .map(ZoneId::of)  // Stream<ZoneId>
-                  .map(now::atZone) // Stream<ZonedDateTime>
-                  .sorted(comparingInt(
-                      zoneId -> zoneId.getOffset().getTotalSeconds()))
-                  .collect(Collectors.toList());
 
-        antarcticZones.forEach(zdt ->
-            System.out.printf("%7s: %25s %7s%n", zdt.getOffset(), zdt.getZone(),
-                zdt.getZone().getRules().isDaylightSavings(zdt.toInstant())));
     }
 }
