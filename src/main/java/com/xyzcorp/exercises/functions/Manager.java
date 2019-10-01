01b2141d20569aeb62a73bbcb4dcd466038f1184
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-10-01 13:27:07 +0200

    Initialized Manager class for functions test

---
+++ src/main/java/com/xyzcorp/exercises/functions/Manager.java
@@ -0,0 +1,41 @@
+package com.xyzcorp.exercises.functions;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Manager extends Employee {
+    private final List<Employee> employees;
+
+    public Manager(String firstName, String lastName, Integer salary,
+                   List<Employee> employees) {
+        super(firstName, lastName, salary);
+        Objects.requireNonNull(employees, "Employees is null");
+        this.employees = employees;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Manager manager = (Manager) o;
+        return employees.equals(manager.employees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), employees);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Manager.class.getSimpleName() + "[", "]")
+            .add("employees=" + employees)
+            .toString();
+    }
+}
