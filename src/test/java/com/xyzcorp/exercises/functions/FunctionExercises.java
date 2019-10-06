fe4d215e4127647c58590a254a106ab83620c767
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-10-06 14:09:00 +0200

    Prepared FunctionExercises for the lab, and moved other lambdas out to its own exercise

--- src/test/java/com/xyzcorp/exercises/functions/FunctionExercises.java
+++ src/test/java/com/xyzcorp/exercises/functions/FunctionExercises.java
@@ -1,5 +1,7 @@
 package com.xyzcorp.exercises.functions;
 
+import com.xyzcorp.exercises.streams.Employee;
+import com.xyzcorp.exercises.streams.Manager;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,27 +10,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FunctionExercises {
-
-    private List<Employee> jkRowlingsEmployees =
-        List.of(
-            new Employee("Harry", "Potter", 30000),
-            new Employee("Hermione", "Granger", 32000),
-            new Employee("Ron", "Weasley", 32000),
-            new Employee("Albus", "Dumbledore", 40000));
-    private Manager jkRowling =
-        new Manager("J.K", "Rowling", 46000, jkRowlingsEmployees);
-
-    private List<Employee> georgeLucasEmployees =
-        List.of(
-            new Employee("Luke", "Skywalker", 33000),
-            new Employee("Princess", "Leia", 36000),
-            new Employee("Han", "Solo", 36000),
-            new Employee("Lando", "Calrissian", 41000));
-    private Manager georgeLucas =
-        new Manager("J.K", "Rowling", 46000, georgeLucasEmployees);
-
-    private List<Manager> managers = List.of(jkRowling, georgeLucas);
-
     /**
      * This is a standard JUnit 5 Test with
      * a standard JUnit5 assertion, you can
