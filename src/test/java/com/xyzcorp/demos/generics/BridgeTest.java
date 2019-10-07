3568c7e3f43c48b1c3758c383e99ac345abfe180
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-10-07 08:34:49 +0300

    added generics tests

---
+++ src/test/java/com/xyzcorp/demos/generics/BridgeTest.java
@@ -0,0 +1,25 @@
+package com.xyzcorp.demos.generics;
+
+import com.xyzcorp.demos.generics.bridge.MyNode;
+import com.xyzcorp.demos.generics.bridge.Node;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeTest {
+
+    /**
+     * The following is an example of a bridge method.
+     * When compiled and running javap -p on the Node and MyNode Types you
+     * will see a more refined type of the method on MyNode that is created
+     * automatically. Run:
+     *
+     * javap -cp target/classes -p com.xyzcorp.bridge.Node
+     */
+    @Test
+    public void testBridgeMethod() {
+        Node<Integer> base = new MyNode(5);
+        Integer data = base.getData();
+        assertThat(data).isEqualTo(5);
+    }
+}
