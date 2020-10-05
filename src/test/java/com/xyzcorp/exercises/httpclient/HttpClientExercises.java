6a66d9e93ca6d0c52ab99f2f45a80deba6a459d9
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-10-04 22:31:59 -0600

    added new material and http client exercise

---
+++ src/test/java/com/xyzcorp/exercises/httpclient/HttpClientExercises.java
@@ -0,0 +1,12 @@
+package com.xyzcorp.exercises.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+public class HttpClientExercises {
+
+    @Test
+    void testReadInJSONPayload() {
+        // Read in information as JSON using Jackson or your favorite JSON Provider:
+        // https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-currency-name.json
+    }
+}
