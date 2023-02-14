14de08e7768b6097812454a4671d3077808d2b02
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-02-14 10:30:57 -0700

    Moved this class back to using synchronized

--- src/main/java/com/xyzcorp/demos/concurrency/deadlock/Friend.java
+++ src/main/java/com/xyzcorp/demos/concurrency/deadlock/Friend.java
@@ -1,45 +1,30 @@
 package com.xyzcorp.demos.concurrency.deadlock;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 class Friend {
     private final String name;
-    private final ReentrantLock lock;
 
-    public Friend(String name, ReentrantLock lock) {
+    public Friend(String name) {
         this.name = name;
-        this.lock = lock;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public void bow(Friend bower) {
-        lock.lock();
-        try {
+    public synchronized void bow(Friend bower) {
             System.out.format("%s in %s: %s  has bowed to me!%n",
                 this.name, Thread.currentThread(), bower.getName());
             bower.bowBack(this);
-        } finally {
-            lock.unlock();
-        }
     }
 
-    public void bowBack(Friend bower) {
-        lock.lock();
-        try {
+    public synchronized void bowBack(Friend bower) {
             System.out.format("%s in %s: %s has bowed back to me!%n",
                 this.name, Thread.currentThread(), bower.getName());
-        } finally {
-            lock.unlock();
-        }
     }
 
     public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock();
-        final Friend alphonse = new Friend("Alphonse", lock);
-        final Friend gaston = new Friend("Gaston", lock);
+        final Friend alphonse = new Friend("Alphonse");
+        final Friend gaston = new Friend("Gaston");
         new Thread(() -> alphonse.bow(gaston), "Alphonse initiates").start(); //alphonse, gaston
         new Thread(() -> gaston.bow(alphonse), "Gaston initiates").start();
     }
