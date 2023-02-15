15eb179c86095fa4022d9c53c144b6fd2fe84b18
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-02-15 15:06:55 -0700

    Added JOL

---
+++ src/main/java/com/xyzcorp/demos/jol/JOLSample_06_HierarchyGaps.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.xyzcorp.demos.jol;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import static java.lang.System.out;
+
+/**
+ * @author Aleksey Shipilev
+ */
+public class JOLSample_06_HierarchyGaps {
+
+    /*
+     * This example shows another HotSpot layout quirk.
+     *
+     * Prior to JDK 15, HotSpot rounds up the instance field block
+     * up to reference size. That unfortunately yields the artificial
+     * gaps at the end of the class.
+     *
+     * In JDK 15 and later, the hierarchy gaps are no longer present.
+     *
+     * See also:
+     *    https://bugs.openjdk.java.net/browse/JDK-8237767
+     */
+
+    public static void main(String[] args) {
+        out.println(VM.current().details());
+        out.println(ClassLayout.parseClass(C.class).toPrintable());
+    }
+
+    public static class A {
+        boolean a;
+    }
+
+    public static class B extends A {
+        boolean b;
+    }
+
+    public static class C extends B {
+        boolean c;
+    }
+
+}
